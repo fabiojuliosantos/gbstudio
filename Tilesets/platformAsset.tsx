c58f5f3cec86a38d37669171fc16c7576556bfef
fabiojuliosantos <fabiojulio.santos@protonmail.ch>
2022-03-15 14:57:13 -0300

    Adição de Tiles/Tilesets

---
+++ Tilesets/platformAsset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="platformAsset" tilewidth="8" tileheight="8" tilecount="360" columns="20">
+ <image source="../Tiles/platformAsset.png" width="160" height="144"/>
+</tileset>
